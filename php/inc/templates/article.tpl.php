a21990cb81d059fc6466afbf2a300f19fffefe58
Oriane-Toque <oriane.toque@gmail.com>
2021-05-10 13:56:26 +0200

    WIN dynamisation un article dans article.tpl.php

--- php/inc/templates/article.tpl.php
+++ php/inc/templates/article.tpl.php
@@ -1,9 +1,68 @@
-<div class="container">
-    <div class="card mb-4">
-        <div class="card-header"><?= $articleToDisplay->title ?></div>
-        <div class="card-body">
-            <p class="card-text"><?= $articleToDisplay->content ?></p>
-            <div class="text-muted">Meta info</div>
+  <!-- Mon container (avec une max-width) dans lequel mon contenu va être placé: https://getbootstrap.com/docs/4.1/layout/overview/#containers -->
+  <div class="container">
+    <!-- Je crée une nouvelle ligne dans ma grille virtuelle: https://getbootstrap.com/docs/4.1/layout/grid/-->
+    <div class="row">
+
+      <!-- Par défaut (= sur mobile) mon element <main> prend toutes les colonnes (=12)
+        MAIS au dela d'une certaine taille, il n'en prendra plus que 9
+        https://getbootstrap.com/docs/4.1/layout/grid/#grid-options -->
+      <main class="col-lg-9">
+
+        <!-- Je dispose une card: https://getbootstrap.com/docs/4.1/components/card/ -->
+        <article class="card">
+          <div class="card-body">
+            <h2 class="card-title"><?= $articleToDisplay->title; ?></h2>
+            <p class="infos">
+              Posté par <a href="#" class="card-link"></a> le <time datetime="<?= $articleToDisplay->date; ?>"><?= $articleToDisplay->date; ?></time> dans <a href="#"
+                class="card-link">#<?= $articleToDisplay->category; ?></a>
+            </p>
+            <p class="card-text"><?= $articleToDisplay->content; ?></p>
+          </div>
+        </article>
+
+        <!-- Je met un element de navigation: https://getbootstrap.com/docs/4.1/components/pagination/ -->
+        <nav aria-label="Page navigation example">
+          <ul class="pagination justify-content-between">
+            <li class="page-item"><a class="page-link" href="index.html"><i class="fa fa-arrow-left"></i> Retour à l'accueil</a></li>
+          </ul>
+        </nav>
+
+      </main>
+
+      <aside class="col-lg-3">
+        <!-- Champ de recherche: https://getbootstrap.com/docs/4.1/components/input-group/ -->
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Rechercher un article..."
+            aria-label="Rechercher un article..." aria-describedby="basic-addon2">
+          <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button">Allez</button>
+          </div>
         </div>
-    </div>
-</div>
+
+        <!-- Catégories: https://getbootstrap.com/docs/4.1/components/card/#list-groups-->
+        <div class="card">
+          <h3 class="card-header">Catégories</h3>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><a href="category.php">TeamBack</a></li>
+            <li class="list-group-item"><a href="category.php">TeamFront</a></li>
+            <li class="list-group-item"><a href="category.php">Collaboration</a></li>
+            <li class="list-group-item"><a href="category.php">Ma Vie De Dev</a></li>
+          </ul>
+        </div>
+
+        <!-- Auteurs: https://getbootstrap.com/docs/4.1/components/card/#list-groups -->
+        <div class="card">
+          <h3 class="card-header">Auteurs</h3>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><a href="#">Maxime</a></li>
+            <li class="list-group-item"><a href="#">Anthony</a></li>
+            <li class="list-group-item"><a href="#">Alexandre</a></li>
+            <li class="list-group-item"><a href="#">Dario</a></li>
+            <li class="list-group-item"><a href="#">Julie</a></li>
+            <li class="list-group-item"><a href="#">Lucie</a></li>
+            <li class="list-group-item"><a href="#">Xavier</a></li>
+          </ul>
+        </div>
+
+      </aside>
+    </div><!-- /.row -->
