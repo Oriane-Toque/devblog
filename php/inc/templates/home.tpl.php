213a76e37c9f6480b9e96f77191728792dc28a22
Oriane-Toque <oriane.toque@gmail.com>
2021-05-10 12:00:22 +0200

    WIN découpage + intégration ok

--- php/inc/templates/home.tpl.php
+++ php/inc/templates/home.tpl.php
@@ -1,57 +1,107 @@
+  <!-- Mon container (avec une max-width) dans lequel mon contenu va être placé: https://getbootstrap.com/docs/4.1/layout/overview/#containers -->
+  <div class="container">
+    <!-- Je crée une nouvelle ligne dans ma grille virtuelle: https://getbootstrap.com/docs/4.1/layout/grid/-->
+    <div class="row">
 
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 col-md-12">
+      <!-- Par défaut (= sur mobile) mon element <main> prend toutes les colonnes (=12)
+        MAIS au dela d'une certaine taille, il n'en prendra plus que 9
+        https://getbootstrap.com/docs/4.1/layout/grid/#grid-options -->
+      <main class="col-lg-9">
+      <!-- <?php foreach($articlesList as $articleId => $articleObject): ?> -->
+        <!-- Je dispose une card: https://getbootstrap.com/docs/4.1/components/card/ -->
+        <article class="card">
+          <div class="card-body">
+            <h2 class="card-title"><a href="index.php?page=article&id=<?= $articleId ?>">Ivre, il refait tous les challenges en un week-end sans dormir.</a></h2>
+            <p class="card-text">Ou comment j'ai appris plein de choses en faisant une nouvelle fois tous les challenges
+              que j'avais loupé.</p>
+            <p class="infos">
+              Posté par <a href="#" class="card-link">Alexandre</a> le <time datetime="2017-07-13">13/07/2017</time> dans <a href="#"
+                class="card-link">#MaVieDeDev</a>
+            </p>
+          </div>
+        </article>
+        <!-- <?php endforeach; ?> -->
+        <!-- Je dispose une card: https://getbootstrap.com/docs/4.1/components/card/ -->
+        <article class="card">
+          <div class="card-body">
+            <h2 class="card-title"><a href="article.html">POO or not POO, that is the question.</a></h2>
+            <p class="card-text">La POO est-elle vraiment indispensable pour une architecture solide ? Etude de cas avec
+              PHP.</p>
+            <p class="infos">
+              Posté par <a href="#" class="card-link">Julie</a> le <time datetime="2017-07-04">04/07/2017</time> dans <a href="#"
+                class="card-link">#TeamBack</a>
+            </p>
+          </div>
+        </article>
+        <!-- Je dispose une card: https://getbootstrap.com/docs/4.1/components/card/ -->
+        <article class="card">
+          <div class="card-body">
+            <h2 class="card-title"><a href="article.html">Git, pour les n00bs.</a></h2>
+            <p class="card-text">Un p'tit récap rapide pour tout ceux qui, comme moi, ont galéré sur ce magnifique outil
+              de versionning.</p>
+            <p class="infos">
+              Posté par <a href="#" class="card-link">Lucie</a> le <time datetime="2017-06-19">19/06/2017</time> dans <a href="#"
+                class="card-link">#Collaboration</a>
+            </p>
+          </div>
+        </article>
+        <!-- Je dispose une card: https://getbootstrap.com/docs/4.1/components/card/ -->
+        <article class="card">
+          <div class="card-body">
+            <h2 class="card-title"><a href="article.html">Le syndrome de la page blanche</a></h2>
+            <p class="card-text">Cette frustration quand le code ne vient pas, le temps passe, la deadline approche...
+            </p>
+            <p class="infos">
+              Posté par <a href="#" class="card-link">Xavier</a> le <time datetime="2017-05-06">06/05/2017</time> dans <a href="#"
+                class="card-link">#MaVieDeDev</a>
+            </p>
+          </div>
+        </article>
 
-                <?php foreach($articlesList as $articleId => $articleObject): ?>
-                    <div class="card mb-4">
-                        <div class="card-header"><a href="index.php?page=article&id=<?= $articleId ?>"><?= $articleObject->title ?></a></div>
-                        <div class="card-body">
-                            <p class="card-text"><?= $articleObject->content ?></p>
-                            <div class="text-muted">Meta info</div>
-                        </div>
-                    </div>
-                <?php endforeach; ?>
+        <!-- Je met un element de navigation: https://getbootstrap.com/docs/4.1/components/pagination/ -->
+        <nav aria-label="Page navigation example">
+          <ul class="pagination justify-content-between">
+            <li class="page-item"><a class="page-link" href="#"><i class="fa fa-arrow-left"></i> Précédent</a></li>
+            <li class="page-item"><a class="page-link" href="#">Suivant <i class="fa fa-arrow-right"></i></a></li>
+          </ul>
+        </nav>
 
-                <nav aria-label="Page navigation" class="mb-4">
-                    <ul class="pagination justify-content-between">
-                        <li class="page-item"><a class="page-link" href="#">Précédent</a></li>
-                        <li class="page-item"><a class="page-link" href="#">Suivant</a></li>
-                    </ul>
-                </nav>
-            </div>
+      </main>
 
-            <div class="col-lg-4 col-md-12">
-                <form class="form mb-4">
-                    <div class="form-row">
-                        <div class="col-7">
-                            <label for="search" class="sr-only">Rechercher</label>
-                            <input type="text" class="form-control mr-2" id="search">
-                        </div>
-                        <div class="col-5">
-                            <button type="submit" class="btn btn-primary form-control">Rechercher</button>
-                        </div>
-                    </div>
-                </form>
-                <!-- emmet : (div.card.mb-4>div.card-header+ul.list-group.list-group-flush>li.list-group-item*4>a)*2 -->
-                <div class="card mb-4">
-                    <div class="card-header">Catégories</div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><a href="category.php">Teamback</a></li>
-                        <li class="list-group-item"><a href="category.php">Teamfront</a></li>
-                        <li class="list-group-item"><a href="category.php">Collaboration</a></li>
-                        <li class="list-group-item"><a href="category.php">Ma vie de dev</a></li>
-                    </ul>
-                </div>
-                <div class="card mb-4">
-                    <div class="card-header">Auteurs</div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><a href="author.php">Maxime</a></li>
-                        <li class="list-group-item"><a href="author.php">Dario</a></li>
-                        <li class="list-group-item"><a href="author.php">Lucie</a></li>
-                        <li class="list-group-item"><a href="author.php">Anthony</a></li>
-                    </ul>
-                </div>
-            </div>
+      <aside class="col-lg-3">
+        <!-- Champ de recherche: https://getbootstrap.com/docs/4.1/components/input-group/ -->
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Rechercher un article..."
+            aria-label="Rechercher un article..." aria-describedby="basic-addon2">
+          <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button">Allez</button>
+          </div>
         </div>
-    </div>
+
+        <!-- Catégories: https://getbootstrap.com/docs/4.1/components/card/#list-groups-->
+        <div class="card">
+          <h3 class="card-header">Catégories</h3>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><a href="category.php">TeamBack</a></li>
+            <li class="list-group-item"><a href="category.php">TeamFront</a></li>
+            <li class="list-group-item"><a href="category.php">Collaboration</a></li>
+            <li class="list-group-item"><a href="category.php">Ma Vie De Dev</a></li>
+          </ul>
+        </div>
+
+        <!-- Auteurs: https://getbootstrap.com/docs/4.1/components/card/#list-groups -->
+        <div class="card">
+          <h3 class="card-header">Auteurs</h3>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><a href="author.php">Maxime</a></li>
+            <li class="list-group-item"><a href="author.php">Anthony</a></li>
+            <li class="list-group-item"><a href="author.php">Alexandre</a></li>
+            <li class="list-group-item"><a href="author.php">Dario</a></li>
+            <li class="list-group-item"><a href="author.php">Julie</a></li>
+            <li class="list-group-item"><a href="author.php">Lucie</a></li>
+            <li class="list-group-item"><a href="author.php">Xavier</a></li>
+          </ul>
+        </div>
+
+      </aside>
+    </div><!-- /.row -->
